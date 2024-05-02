37f9299759b9dd5ac0e59c1f9c96df81750d39ae
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-02 16:25:15 -0400

    added a template literal for License information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,7 +11,9 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `This application is licensed under the ${license} license.`
+}
 
 
 // TODO: Create a function to generate markdown for README

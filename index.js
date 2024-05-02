7965fd82a2cd4174d5e81bc611427dec837c367a
jdmonticelli <jdmonticelli@gmail.com>
2024-05-01 23:06:29 -0400

    added functions code to index.js

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown=require("./utils/generateMarkdown")
+const fs=require("fs")
+const inquirer= require("inquirer")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type:'input',
+    message:"What is your proect title?",
+    name:'title'
+},{
+    type:'list',
+    message:"What is your proect title?",
+    name:'license',
+    choices:["MIT","Apache","IBM","No License"]
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const content=generateMarkdown(data)    
+    fs.writeFile(fileName,content,err=>{
+        if(err){
+            console.error(err)
+            return
+        }
+        console.log("Success!")
+        
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt( questions)
+    .then(data=>{
+        writeToFile("./output/README.md",data)
+    }) 
+
+}
 
 // Function call to initialize app
 init();
